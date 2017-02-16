616ed618462737a2412d5c8b4354f7d9b95b87f9
Daniel Passos <daniel@passos.me>
2017-02-16 21:24:31 -0200

    Add drawer menu

---
+++ mobile/src/main/java/org/devnexus/HomeFragment.java
@@ -0,0 +1,20 @@
+package org.devnexus;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext())
+                .inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+}
--- mobile/src/main/java/org/devnexus/MainActivity.java
+++ mobile/src/main/java/org/devnexus/MainActivity.java
@@ -1,13 +1,111 @@
 package org.devnexus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.mikepenz.aboutlibraries.LibsBuilder;
+import com.mikepenz.aboutlibraries.ui.LibsFragment;
+import com.mikepenz.aboutlibraries.ui.LibsSupportFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.menu)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        setupNavigationMenu();
+        displayHome();
     }
+
+    // -- Navigation ------------------------------------------------------------------------------
+
+    @OnClick(R.id.drawer)
+    void openDrawerMenu() {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    private void setupNavigationMenu() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        displayHome();
+                        break;
+                    case R.id.all_presentations:
+                        displayPresentations();
+                        break;
+                    case R.id.my_schedule:
+                        displayMySchedule();
+                        break;
+                    case R.id.scan_badge:
+                        displayScanBadge();
+                        break;
+                    case R.id.about:
+                        displayAbout();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    private void display(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, fragment)
+                .commit();
+    }
+
+    private void displayHome() {
+        display(new HomeFragment());
+    }
+
+    private void displayPresentations() {
+        display(new PresentationsFragment());
+    }
+
+    private void displayMySchedule() {
+        display(new MyScheduleFragment());
+    }
+
+    private void displayScanBadge() {
+        display(new ScanBadgeFragment());
+    }
+
+    private void displayAbout() {
+        LibsSupportFragment fragment = new LibsBuilder()
+                .withAboutIconShown(true)
+                .withAboutVersionShown(true)
+                .withAboutDescription(getString(R.string.about_description))
+                .supportFragment();
+        display(fragment);
+    }
+
 }
